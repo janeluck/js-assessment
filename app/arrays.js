7a388002540f7259ee8220ef6173e502a47c6e74
janeluck <janeluck158@gmail.com>
2015-11-06 13:07:00 +0800

    update anwsers

--- app/arrays.js
+++ app/arrays.js
@@ -37,31 +37,39 @@ exports.arraysAnswers = {
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
+    arr.pop();
+    return arr;
 
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert : function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count : function(arr, item) {
-
+    return arr.filter(function(el){
+      return el === item;
+    }).length;
   },
 
   duplicates : function(arr) {
